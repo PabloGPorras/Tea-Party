0dafc8fdae2614dc434bf016d0a50159e2330de6
pablo <pablogeorgeporras@gmail.com>
2022-09-26 14:25:45 -0700

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,36 +7,26 @@
 
   const apiBasedUrl = "https://ebay-backend-django.herokuapp.com";
   let data = "";
-
+  let orders = "";
   onMount(async () => {
     const res = await fetch(apiBasedUrl + "/ebay/");
     data = await res.json();
   });
 
   const getOrders = async (code) => {
-    const res = await fetch(apiBasedUrl + "/ebay/getUser/" + code);
-    data = await res.json();
+    const res = await fetch(apiBasedUrl + `/ebay/${code}/getOrders/`);
+    orders = await res.json();
   };
+  //<button type="submit" onclick={getOrders(code)}>Get Orders </button>
 </script>
 
-<h1>Access_token:{code}</h1>
-
-<h1>Clicky the linky ;)</h1>
 <h1>{data.signin_url}</h1>
-<button
-  class="btn waves-effect waves-light btn-large"
-  type="submit"
-  name="action"
-  onclick="window.location.href = '{data.signin_url}';"
+<button type="submit" onclick="window.location.href = '{data.signin_url}';"
   >Get User Access Token
 </button>
 
-<button
-  class="btn waves-effect waves-light btn-large"
-  type="submit"
-  name="action"
-  onclick={data.signin_url}
-  >Get Orders
-</button>
+<h1>Access_token:{code}</h1>
+
+<button on:click={getOrders(code)}> Get Orders </button>
 
 <h1>orders:{orders}</h1>
