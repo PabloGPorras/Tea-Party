3d0f8af2d333597487c55133dfbca090027f23ae
pablo <pablogeorgeporras@gmail.com>
2022-09-26 12:51:00 -0700

    Can get the query param now and added the click the link button

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { page } from "$app/stores";
+  const { url, param } = $page;
+  const code = url.searchParams.get("code");
+
+  const apiBasedUrl = "https://ebay-backend-django.herokuapp.com";
+  let data = "";
+
+  onMount(async () => {
+    const res = await fetch(apiBasedUrl + "/ebay/");
+    data = await res.json();
+    console.log("Hello World2");
+    console.log(data.signin_url);
+  });
+</script>
+
+<h1>{code}</h1>
+
+<h1>Clicky the linky ;)</h1>
+<h1>{data.signin_url}</h1>
+<button
+  class="btn waves-effect waves-light btn-large"
+  type="submit"
+  name="action"
+  onclick="window.location.href = '{data.signin_url}';"
+  >Submit
+</button>
