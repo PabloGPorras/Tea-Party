a68d2de08cb637c6ced4a016687d7443a36a6906
pablo <pablogeorgeporras@gmail.com>
2022-09-28 12:48:34 -0700

    testing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,21 +12,24 @@
   const apiBasedUrl = "https://ebay-backend-django.herokuapp.com";
   let data = "";
   let orders = "Click Grab Order";
-
+  let userData = "Click Get User Data";
   onMount(async () => {
     const res = await fetch(apiBasedUrl + "/ebay/");
     data = await res.json();
   });
 
   const getOrders = async (token) => {
-    console.log("getOrders token: " + token);
-    //let getOrdersUrl = apiBasedUrl + "/ebay/getOrders/?code=" + token;
-    let getOrdersUrl = apiBasedUrl + "/ebay/" + token + "/90/getOrders/";
-    console.log("getOrdersUrl: " + getOrdersUrl);
-
+    let getOrdersUrl = apiBasedUrl + "/ebay/" + token + "/30/getOrders/";
     const res = await fetch(getOrdersUrl);
     orders = await res.json();
     orders = orders.orders;
+  };
+
+  const getUsers = async (token) => {
+    let getOrdersUrl = apiBasedUrl + "/ebay/" + token + "/getUsers/";
+    const res = await fetch(getOrdersUrl);
+    userData = await res.json();
+    console.log(userData);
   };
 </script>
 
@@ -34,9 +37,10 @@
 <button type="submit" onclick="window.location.href = '{data.signin_url}';"
   >Get User Access Token
 </button>
-
 <h1>token:{token}</h1>
 
 <button on:click={getOrders(token)}> Get Orders </button>
-
 <h1>orders:{orders}</h1>
+
+<button on:click={getUsers(token)}> Get User Data </button>
+<h1>orders:{userData}</h1>
